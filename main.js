d19d60a8b00dedf27add9da541d541115160b8c4
Amneisa <hamwif200@outlook.com>
2024-10-29 11:29:04 +0300

    finishing article

---
+++ main.js
@@ -0,0 +1,10 @@
+const shareBtn = document.querySelector('.share-btn')
+const shareMenu = document.querySelector('.share-menu')
+
+shareBtn.addEventListener('click', (e) =>{
+    shareMenu.classList.toggle('hide')
+})
+
+
+
+
